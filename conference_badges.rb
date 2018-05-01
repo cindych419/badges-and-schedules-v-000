4116f14bebbee7a1d2b0926715994d0614bb2b65
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 20:03:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,14 +15,14 @@ def assign_rooms(attendees)
 end
 end
 
-def printer(batch_badge_creator)
-  batch_badge_creator.each do |badge|
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
 end
 
-def printer(assign_rooms)
-  assign_rooms.each do |roomassingment|
+def printer(attendees)
+  assign_rooms(attendees).each do |roomassingment|
     puts roomassingment
   end
 end
