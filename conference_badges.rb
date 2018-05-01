607714fa5363da81cd6944b086d0f8a45926253a
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 19:31:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(list)
+  list.map do |attendee|
+  "Hello, my name is #{attendee}."
+end
+end
+
+def assign_rooms(list)
+counter = 0
+list.map do |attendee|
+  "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+  counter +=1
+end
+end
