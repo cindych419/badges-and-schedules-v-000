6ee1dc1ae733a78e047828411f3629b36a6e7950
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 19:38:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
+  attendees.map do |newmessage|
+    "Hello, my name is #{newmessage}."
+  end
+end
