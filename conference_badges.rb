a7e18bb77f70d5c5ab6e41283afb18a5085467eb
cindych419@gmail.com <cindych419@gmail.com>
2018-05-01 20:00:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,8 @@ def assign_rooms(attendees)
   "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 end
+
+def printer(batch_badge_creator)
+  batch_badge_creator.each do |badge|
+    puts badge
+  end 
